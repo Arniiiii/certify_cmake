b7131d1e9e486608cea300c139f61fba51453775
Arniiiii <lg3dx6fd@gmail.com>
2024-08-30 20:53:08 +0000

    little fix

--- cmake/add_boost.cmake
+++ cmake/add_boost.cmake
@@ -1,6 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/getCPM.cmake)
 
-# cpmaddpackage( NAME AddBoost.CMake VERSION 3.3 URL
-# https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/3.1.tar.gz)
-cpmaddpackage( NAME AddBoost.CMake VERSION 3.3 URL
-  GITHUB_REPOSITORY Arniiiii/AddBoost.cmake GIT_TAG testing)
+cpmaddpackage(
+  NAME AddBoost.CMake VERSION 3.3 URL
+  https://github.com/Arniiiii/AddBoost.cmake/archive/refs/tags/3.3.tar.gz)
+# cpmaddpackage( NAME AddBoost.CMake VERSION 3.3 URL GITHUB_REPOSITORY
+# Arniiiii/AddBoost.cmake GIT_TAG testing)
